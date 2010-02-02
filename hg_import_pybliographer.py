8f8490cffe9b3c8ddcae2354ad7e0354162e5190
Matthew Brett <matthew.brett@gmail.com>
2010-02-02 12:02:52 +0000

    Initial code import

---
+++ hg_import_pybliographer.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+
+import os
+from os.path import join as pjoin, abspath, isdir, isfile, dirname
+import sys
+import subprocess
+import tla2hg_hist as tla
+reload(tla)
+from tla2hg_hist import  shcall, shrun
+
+
+arch_cmd = 'tla'
+archives = ('2007', '2006', '2005', 'public')
+# from tla abrowse ftp://arch.pybliographer.org/archives/2007
+'''
+gobry@pybliographer.org--2007
+  pyblio
+    pyblio--devel
+      pyblio--devel--1.3
+        base-0 .. patch-23
+
+    pyblio--legacy
+      pyblio--legacy--1.3
+        base-0 .. version-0
+
+    pyblio--stable
+      pyblio--stable--1.2
+        base-0 .. patch-30
+
+  pyblio-core
+    pyblio-core--devel
+      pyblio-core--devel--1.3
+        base-0 .. patch-29
+
+  python-bibtex
+    python-bibtex--devel
+      python-bibtex--devel--1.3
+        base-0
+
+    python-bibtex--stable
+      python-bibtex--stable--1.2
+        base-0
+'''
+projects = (
+    ('pyblio--devel--1.3', 'pyblio-devel-1.3'),
+    )
+
+# Set our own archive command to module global
+tla.archcmd = arch_cmd
+
+
+def pyblio_archive_map(archives, base_location, source=True):
+    if source:
+        suffix = '-SOURCE'
+    else:
+        suffix = ''
+    archive_map = {}
+    for archive in archives:
+        location = os.path.join(base_location, archive)
+        name = 'gobry@pybliographer.org--%s%s' % (archive, suffix)
+        archive_map[archive] = (name, location)
+    return archive_map
+
+
+def register_archives(archive_map, remove=False):
+    if remove:
+        flag = '-d'
+    else:
+        flag = ''
+    for code, name_location in archive_map.items():
+        name, location = name_location
+        shcall('%s register-archive %s %s %s' % (
+                arch_cmd, flag, name, location),
+               check=False)
+
+
+def clear_archives():
+    arch_names = shrun('tla archives --names').split('\n')
+    for name in [n for n in arch_names if n]:
+        shcall('tla register-archive -d ' + name)
+
+
+def mirror_archives(archive_map, archive_dir, source_suffix='-SOURCE'):
+    # http://www.enyo.de/fw/software/arch/get.html#6
+    archive_dir = os.path.abspath(archive_dir)
+    new_map = {}
+    for code, name_location in archive_map.items():
+        name, location = name_location
+        new_name = name
+        if new_name.endswith(source_suffix):
+            if len(source_suffix):
+                new_name = new_name[:-len(source_suffix)]
+        new_location = os.path.join(archive_dir, code)
+        shcall('tla make-archive '
+               '--mirror-from %s %s' % (name, new_location),
+               check=False)
+        shcall('tla archive-mirror %s' % new_name, check=False)
+        new_map[code] = (new_name, new_location)
+    return new_map
+
+
+def set_default_archive(archive_name):
+    shcall('%s my-default-archive %s'
+           % (arch_cmd, archive_name))
+
+
+def import_projects(projects):
+    for arch_proj, hg_proj in projects:
+        tla.tla_to_hg(arch_proj, hg_proj)
+
+
+if __name__ == '__main__':
+    # don't forget to set an hg username
+    clear_archives()
+    shcall('tla my-id "Matthew Brett <matthew.brett@gmail.com>"')
+    #archive_dir = os.path.abspath('archives')
+    amap = pyblio_archive_map(archives,
+                              'ftp://arch.pybliographer.org/archives',
+                              source=False)
+    #amap = pyblio_archive_map(archives,
+    #                          '/Users/mb312/dev_trees/pybliographer/mirror',
+    #                          source=False)
+    register_archives(amap)
+    #mirror_archives(amap, archive_dir)
+    #amap = pyblio_archive_map(archives,
+    #                          archive_dir,
+    #                          source=False)
+    #register_archives(amap, remove=True)
+    set_default_archive(amap['2007'][0])
+    #import_projects(projects)
+    revs = tla.get_revisions('pyblio--devel--1.3')
+    
---
+++ tla2hg_hist.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+"""
+   Convert a repository to mercurial (hg).
+
+   http://www.linux-france.org/~dmentre/misc/tla-to-hg-hist.py
+   
+   This runs with baz 1.1.1  and also with tla 1.3-1 - if you do changes,
+   maybe either try to avoid stuff requiring latest versions (it means
+   much trouble for people trying to convert).
+
+   TODO:
+    * test reading tla multiline comments (had no testcase)
+    * commit timestamps are UTC now, maybe use localtime?
+
+   @license: BSD ???
+   @copyright: 2005 Sam Tardieu
+   @copyright: 2005 Ollivier Robert
+   @copyright: 2005 Thomas Waldmann (rewrite, optimize)
+   @copyright: 2006 David MENTRE (full history, tested with tla 1.3)
+"""
+import os, sys, time
+import subprocess
+
+
+# it works with either one:
+archcmd = "tla"
+#archcmd = "baz"
+
+
+def shcall(cmd, check=True):
+    if check:
+        return subprocess.check_call(cmd, shell=True)
+    return subprocess.call(cmd, shell=True)
+
+    
+def shrun(cmd, ret_error=False, ret_code=False):
+    proc = subprocess.Popen(cmd,
+                 stdout=subprocess.PIPE,
+                 stderr=subprocess.PIPE,
+                 shell=True)
+    (out, err) = proc.communicate()
+    if not (ret_error or ret_code):
+        return out
+    ret = [out]
+    if ret_error:
+        ret.append(err)
+    if ret_code:
+        ret.append(proc.returncode)
+    return ret
+
+
+def get_revisions(archive):
+    """get revision list of archive"""
+    shcall("%s get %s tmp-archive" % (archcmd, archive))
+    revlist = shrun("cd tmp-archive && %s ancestry-graph --reverse"
+                    % archcmd)
+    os.remove('tmp-archive')
+    revlist = [r for r in revlist.split('\n') if r]
+    version_list = []
+    for r in revlist:
+        rev = r.split('\t')[0]
+        fullrev = "%s--%s" % (rev.strip(), archive)
+        version_list.append(fullrev)
+    return version_list
+
+
+def read_summary(fullrev):
+    author = ""
+    date = ""
+    summary = []
+    skip_line = False
+    for l in shrun("%s cat-archive-log %s" % (archcmd, fullrev)).split('\n'):
+        if len(summary) == 0:
+            if l.startswith("Creator: "):
+                author = l[9:].strip()
+            elif l.startswith("Standard-date: "): # this is UTC
+                datestr = l[15:].strip()
+                t = time.mktime(time.strptime(datestr, "%Y-%m-%d %H:%M:%S %Z"))
+                date = "%d 0" % int(t)
+            elif l.startswith("Summary: "):
+                summary = [l[9:].strip()]
+                skip_line = True
+        elif skip_line: # skip one line
+            skip_line = False
+        else: # we have begun reading the summary, continues until eof...
+            stripped = l.rstrip()
+            if l and stripped != summary[0]:
+                summary.append(stripped)
+    summary.extend(["", "imported from: %s" % fullrev])
+    return summary, author, date
+
+
+def commit_log(fullrev, mercurial_dir):
+    summary, author, date = read_summary(fullrev)
+    try:
+        fd = open('tmp-msg', 'w')
+        fd.write('\n'.join(summary) + '\n')
+        fd.close()
+        shcall("cd %s && hg commit "
+               "--addremove -l ../tmp-msg --date '%s' --user '%s'"
+               % (mercurial_dir, date, author))
+    finally:
+        os.remove('tmp-msg')
+
+
+def make_initial_revision(fullrev, mercurial_dir):
+    """make initial hg repository"""
+    sys.stdout.write(">>> '%s'\n" % fullrev)
+    shcall("%s get %s %s" % (archcmd, fullrev, mercurial_dir))
+    hgi = open("%s/.hgignore" % mercurial_dir, "w")
+    hgi.write("""\
+.arch-ids/.*
+\{arch\}/.*
+""")
+    hgi.close()
+    shcall("cd %s && hg init" % mercurial_dir)
+    commit_log(fullrev, mercurial_dir)
+
+
+def import_revision(archive, fullrev, mercurial_dir):
+    """import a single arch revision into an hg repo"""
+    sys.stdout.write(">>> '%s'\n" % fullrev)
+    shcall("cd %s && %s replay %s" % (mercurial_dir, archcmd, fullrev))
+    commit_log(fullrev, mercurial_dir)
+
+
+def tla_to_hg(archive, mercurial_dir):
+    if not os.path.exists(mercurial_dir):
+        os.mkdir(mercurial_dir)
+    version_list = get_revisions(archive)
+    make_initial_revision(version_list[0], mercurial_dir)
+    for fullrev in version_list[1:]:
+        import_revision(archive, fullrev, mercurial_dir)
+
+
+if __name__ == '__main__':
+    try:
+        cmd, archive, mercurial_dir = sys.argv[:3]
+    except IndexError:
+        sys.stdout.write("""\
+    Usage: arch-to-hg.py archive target_dir
+
+    archive: the tla/baz archive you are converting from
+    target_dir: where to put conversion results in hg format, must be some
+                directory in the current directory, like "outputdir".
+    """)
+    tla_to_hg(archive, mercurial_dir)
+    
+
